147db825250a966a542e98414915e398fc910304
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-03-02 22:48:13 +0300

    data inserted for mongo db

--- EasyDebuger/data/index.js
+++ EasyDebuger/data/index.js
@@ -1,14 +1,14 @@
 import javascriptErrors from "./javascript.json";
 import reactErrors from "./react.json";
-// import nodejsErrors from "./nodejs.json";
-// import mongodbErrors from "./mongodb.json";
+import nodejsErrors from "./nodejs.json";
+import mongodbErrors from "./mongodb.json";
 // import pythonErrors from "./python.json";
 
 const errorData = {
   javascript: javascriptErrors,
   react: reactErrors,
-  // nodejs: nodejsErrors,
-  // mongodb: mongodbErrors,
+  nodejs: nodejsErrors,
+  mongodb: mongodbErrors,
   // python: pythonErrors,
 };
 
