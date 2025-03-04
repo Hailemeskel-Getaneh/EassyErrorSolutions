11f8a79599b7afdbd302ceb1e85673c91666c50c
Hailemeskel Getaneh <hailegetaneh1221@gmail.com>
2025-03-04 14:05:58 +0300

    data inserted for python

--- EasyDebuger/data/index.js
+++ EasyDebuger/data/index.js
@@ -2,14 +2,22 @@ import javascriptErrors from "./javascript.json";
 import reactErrors from "./react.json";
 import nodejsErrors from "./nodejs.json";
 import mongodbErrors from "./mongodb.json";
-// import pythonErrors from "./python.json";
+import pythonErrors from "./python.json";
+import gitErrors from "./git_and_github.json";
+import reactNativeErrors from "./react_native.json";
+import javaErrors from "./java.json";
+import cppErrors from "./cpp.json";
 
 const errorData = {
   javascript: javascriptErrors,
   react: reactErrors,
   nodejs: nodejsErrors,
   mongodb: mongodbErrors,
-  // python: pythonErrors,
+  python: pythonErrors,
+  git: gitErrors,
+  reactNative: reactNativeErrors,
+  java: javaErrors,
+  
 };
 
 export default errorData;
